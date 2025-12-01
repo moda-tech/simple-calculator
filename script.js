b1e5abb3d0a6f09af356c2c824fb11bbc703272f
Shimoda <aocha0705dayo@gmail.com>
2025-12-01 10:26:07 +0900

    修正済、テスト前

--- script.js
+++ script.js
@@ -18,214 +18,260 @@ const n00 = document.getElementById("n00")
 const n0 = document.getElementById("n0")
 const equal = document.getElementById("equal")
 
-let num1 = "";
-let num2 = "";
-let state = a;
+let num1 = [];
+let num2 = [];
+let state = "a";
 let cal = "";
-sum.textContent = num1
+sum.textContent = num1.join("");
 
 n1.addEventListener("click", () => {
     if(state == "a"){
         num1.push("1")
+        sum.textContent = num1.join("");
     }
-    if(state == "b"){
+    else if(state == "b"){
         num2.push("1")
         state = "c"
-        sum.textContent = num2
+        sum.textContent = num2.join("");
     }
-    if(state == "c"){
+    else if(state == "c"){
         num2.push("1")
+        sum.textContent = num2.join("");
     }
-    if(state == "d"){
-        num1 = ""
+    else if(state == "d"){
+        num1 = [];
         num1.push("1")
+        sum.textContent = num1.join("");
+        state = "a"
     }
     
 });
 n2.addEventListener("click", () => {
     if(state == "a"){
         num1.push("2")
+        sum.textContent = num1.join("");
     }
-    if(state == "b"){
+    else if(state == "b"){
         num2.push("2")
         state = "c"
-        sum.textContent = num2
+        sum.textContent = num2.join("");
     }
-    if(state == "c"){
+    else if(state == "c"){
         num2.push("2")
+        sum.textContent = num2.join("");
     }
-    if(state == "d"){
-        num1 = ""
+    else if(state == "d"){
+        num1 = [];
         num1.push("2")
+        sum.textContent = num1.join("");
+        state = "a"
     }
 });
 n3.addEventListener("click", () => {
     if(state == "a"){
         num1.push("3")
+        sum.textContent = num1.join("");
     }
-    if(state == "b"){
+    else if(state == "b"){
         num2.push("3")
         state = "c"
-        sum.textContent = num2
+        sum.textContent = num2.join("");
     }
-    if(state == "c"){
+    else if(state == "c"){
         num2.push("3")
+        sum.textContent = num2.join("");
     }
-    if(state == "d"){
-        num1 = ""
+    else if(state == "d"){
+        num1 = [];
         num1.push("3")
+        sum.textContent = num1.join("");
+        state = "a"
     }
 });
 n4.addEventListener("click", () => {
     if(state == "a"){
         num1.push("4")
+        sum.textContent = num1.join("");
     }
-    if(state == "b"){
+    else if(state == "b"){
         num2.push("4")
         state = "c"
-        sum.textContent = num2
+        sum.textContent = num2.join("");
     }
-    if(state == "c"){
+    else if(state == "c"){
         num2.push("4")
+        sum.textContent = num2.join("");
     }
-    if(state == "d"){
-        num1 = ""
+    else if(state == "d"){
+        num1 = [];
         num1.push("4")
+        sum.textContent = num1.join("");
+        state = "a"
     }
 });
 n5.addEventListener("click", () => {
     if(state == "a"){
         num1.push("5")
+        sum.textContent = num1.join("");
     }
-    if(state == "b"){
+    else if(state == "b"){
         num2.push("5")
         state = "c"
-        sum.textContent = num2
+        sum.textContent = num2.join("");
     }
-    if(state == "c"){
+    else if(state == "c"){
         num2.push("5")
+        sum.textContent = num2.join("");
     }
-    if(state == "d"){
-        num1 = ""
+    else if(state == "d"){
+        num1 = [];
         num1.push("5")
+        sum.textContent = num1.join("");
+        state = "a"
     }
 });
 n6.addEventListener("click", () => {
     if(state == "a"){
         num1.push("6")
+        sum.textContent = num1.join("");
     }
-    if(state == "b"){
+    else if(state == "b"){
         num2.push("6")
         state = "c"
-        sum.textContent = num2
+        sum.textContent = num2.join("");
     }
-    if(state == "c"){
+    else if(state == "c"){
         num2.push("6")
+        sum.textContent = num2.join("");
     }
-    if(state == "d"){
-        num1 = ""
+    else if(state == "d"){
+        num1 = [];
         num1.push("6")
+        sum.textContent = num1.join("");
+        state = "a"
     }
 });
 n7.addEventListener("click", () => {
     if(state == "a"){
         num1.push("7")
+        sum.textContent = num1.join("");
     }
-    if(state == "b"){
+    else if(state == "b"){
         num2.push("7")
         state = "c"
-        sum.textContent = num2
+        sum.textContent = num2.join("");
     }
-    if(state == "c"){
+    else if(state == "c"){
         num2.push("7")
+        sum.textContent = num2.join("");
     }
-    if(state == "d"){
-        num1 = ""
+    else if(state == "d"){
+        num1 = [];
         num1.push("7")
+        sum.textContent = num1.join("");
+        state = "a"
     }
 });
 n8.addEventListener("click", () => {
     if(state == "a"){
         num1.push("8")
+        sum.textContent = num1.join("");
     }
-    if(state == "b"){
+    else if(state == "b"){
         num2.push("8")
         state = "c"
-        sum.textContent = num2
+        sum.textContent = num2.join("");
     }
-    if(state == "c"){
+    else if(state == "c"){
         num2.push("8")
+        sum.textContent = num2.join("");
     }
-    if(state == "d"){
-        num1 = ""
+    else if(state == "d"){
+        num1 = [];
         num1.push("8")
+        sum.textContent = num1.join("");
+        state = "a"
     }
 });
 n9.addEventListener("click", () => {
     if(state == "a"){
         num1.push("9")
+        sum.textContent = num1.join("");
     }
-    if(state == "b"){
+    else if(state == "b"){
         num2.push("9")
         state = "c"
-        sum.textContent = num2
+        sum.textContent = num2.join("");
     }
-    if(state == "c"){
+    else if(state == "c"){
         num2.push("9")
+        sum.textContent = num2.join("");
     }
-    if(state == "d"){
-        num1 = ""
+    else if(state == "d"){
+        num1 = [];
         num1.push("9")
+        sum.textContent = num1.join("");
+        state = "a"
     }
 });
 n0.addEventListener("click", () => {
     if(state == "a"){
         num1.push("0")
+        sum.textContent = num1.join("");
     }
-    if(state == "b"){
+    else if(state == "b"){
         num2.push("0")
         state = "c"
-        sum.textContent = num2
+        sum.textContent = num2.join("");
     }
-    if(state == "c"){
+    else if(state == "c"){
         num2.push("0")
+        sum.textContent = num2.join("");
     }
-    if(state == "d"){
-        num1 = ""
+    else if(state == "d"){
+        num1 = [];
         num1.push("0")
+        sum.textContent = num1.join("");
+        state = "a"
     }
 });
 n00.addEventListener("click", () => {
     if(state == "a"){
         num1.push("00")
+        sum.textContent = num1.join("");
     }
-    if(state == "b"){
+    else if(state == "b"){
         num2.push("00")
         state = "c"
-        sum.textContent = num2
+        sum.textContent = num2.join("");
     }
-    if(state == "c"){
+    else if(state == "c"){
         num2.push("00")
+        sum.textContent = num2.join("");
     }
-    if(state == "d"){
-        num1 = ""
+    else if(state == "d"){
+        num1 = [];
         num1.push("00")
+        sum.textContent = num1.join("");
+        state = "a"
     }
 });
 
 clear.addEventListener("click", () => {
-    num1 = "";
-    num2 = "";
+    num1 = [];
+    num2 = [];
     state = "a"
-    sum.textContent = num1
+    sum.textContent = num1.join("");
 });
 
 del.addEventListener("click", () => {
     if(state == "a"){
         num1 = num1.slice(0, -1);
+        sum.textContent = num1.join("");
     }
     if(state == "c"){
         num2 = num1.slice(0, -1);
+        sum.textContent = num2.join("");
     }
 });
 
@@ -236,6 +282,8 @@ waru.addEventListener("click", () => {
     }else if(state == "b"){
         cal = "waru";
     }else if(state == "c"){
+        num1 = num1.join("")
+        num2 = num2.join("")
         if(cal == "tasu"){
             num1 = Number(num1) + Number(num2)
         }else if(cal == "hiku"){
@@ -243,12 +291,13 @@ waru.addEventListener("click", () => {
         }else if(cal == "kakeru"){
             num1 = Number(num1) * Number(num2)
         }else if(cal == "waru"){
-            num1 = Number(num2) / Number(num1)
+            num1 = Number(num1) / Number(num2)
         };
-        num2 = 0
+        num2 = [];
         state = "b"
         cal = "waru"
-        sum.textContent = num1;
+        sum.textContent = num1
+        num1 = String(num1).split("");
     }else if(state == "d"){
         cal = "waru";
         state = "b"
@@ -261,6 +310,8 @@ kakeru.addEventListener("click", () => {
     }else if(state == "b"){
         cal = "kakeru";
     }else if(state == "c"){
+        num1 = num1.join("")
+        num2 = num2.join("")
         if(cal == "tasu"){
             num1 = Number(num1) + Number(num2)
         }else if(cal == "hiku"){
@@ -268,12 +319,13 @@ kakeru.addEventListener("click", () => {
         }else if(cal == "kakeru"){
             num1 = Number(num1) * Number(num2)
         }else if(cal == "waru"){
-            num1 = Number(num2) / Number(num1)
+            num1 = Number(num1) / Number(num2)
         };
-        num2 = 0
+        num2 = [];
         state = "b"
         cal = "kakeru"
-        sum.textContent = num1;
+        sum.textContent = num1
+        num1 = String(num1).split("");
     }else if(state == "d"){
         cal = "kakeru";
         state = "b"
@@ -286,6 +338,8 @@ hiku.addEventListener("click", () => {
     }else if(state == "b"){
         cal = "hiku";
     }else if(state == "c"){
+        num1 = num1.join("")
+        num2 = num2.join("")
          if(cal == "tasu"){
             num1 = Number(num1) + Number(num2)
         }else if(cal == "hiku"){
@@ -293,12 +347,13 @@ hiku.addEventListener("click", () => {
         }else if(cal == "kakeru"){
             num1 = Number(num1) * Number(num2)
         }else if(cal == "waru"){
-            num1 = Number(num2) / Number(num1)
+            num1 = Number(num1) / Number(num2)
         };
-        num2 = 0
+        num2 = [];
         state = "b"
         cal = "hiku"
-        sum.textContent = num1;
+        sum.textContent = num1
+        num1 = String(num1).split("");
     }else if(state == "d"){
         cal = "hiku";
         state = "b"
@@ -311,6 +366,8 @@ tasu.addEventListener("click", () => {
     }else if(state == "b"){
         cal = "tasu";
     }else if(state == "c"){
+        num1 = num1.join("")
+        num2 = num2.join("")
         if(cal == "tasu"){
             num1 = Number(num1) + Number(num2)
         }else if(cal == "hiku"){
@@ -318,12 +375,13 @@ tasu.addEventListener("click", () => {
         }else if(cal == "kakeru"){
             num1 = Number(num1) * Number(num2)
         }else if(cal == "waru"){
-            num1 = Number(num2) / Number(num1)
+            num1 = Number(num1) / Number(num2)
         };
-        num2 = 0
+        num2 = [];
         state = "b"
         cal = "tasu"
-        sum.textContent = num1;
+        sum.textContent = num1
+        num1 = String(num1).split("");
     }else if(state == "d"){
         cal = "tasu";
         state = "b"
@@ -332,6 +390,10 @@ tasu.addEventListener("click", () => {
 
 equal.addEventListener("click", () => {
     if(state == "c"){
+        num1 = num1.join("")
+        num2 = num2.join("")
+        console.log(num1)
+        console.log(num2)
          if(cal == "tasu"){
             num1 = Number(num1) + Number(num2)
         }else if(cal == "hiku"){
@@ -339,11 +401,13 @@ equal.addEventListener("click", () => {
         }else if(cal == "kakeru"){
             num1 = Number(num1) * Number(num2)
         }else if(cal == "waru"){
-            num1 = Number(num2) / Number(num1)
+            num1 = Number(num1) / Number(num2)
         };
-        num2 = 0
+        num2 = [];
         state = "d"
         cal = ""
-        sum.textContent = num1;
+        sum.textContent = num1
+        num1 = String(num1).split("");
+        console.log(num1)
     }
 });
